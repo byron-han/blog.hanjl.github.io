5f330f4b0f8ab3af662df12cc38d3015d5c717a3
HANJINGLEI <h@hanjl.com>
2022-09-23 11:39:30 +0800

    feat(base): 基础环境搭建

--- sw.js
+++ sw.js
@@ -31,8 +31,9 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "blog.hanjl.com",
+  "hanjl.com",
+  "byron-han.github.io",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
